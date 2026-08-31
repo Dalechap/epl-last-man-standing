fa25fd258c637080a220f0f3534fcca1eba151fa
Dalechap <dale.chapple22@gmail.com>
2026-08-31 16:52:33 +0700

    Update football.js

--- api/football.js
+++ api/football.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const round = Number(req.query.round);
+    const round = Number(req.query.matchday);
 
     if (!round) {
       return res.status(400).json({ error: 'Round number is required' });
