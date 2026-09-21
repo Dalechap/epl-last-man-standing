13328a7a4cf80cdf2d4abf95c1964a4fd8dcb338
Dalechap <dale.chapple22@gmail.com>
2026-09-21 11:32:31 +0700

    Create sw.js
    Add notification service worker

---
+++ sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('push', event => {
+  const data = event.data
+    ? event.data.json()
+    : {};
+
+  const title =
+    data.title || 'Last Man Standing';
+
+  const options = {
+    body: data.body || '',
+    icon: '/EPL.jpg',
+    badge: '/EPL.jpg',
+    data: {
+      url: data.url || '/'
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(
+      title,
+      options
+    )
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+
+  const url =
+    event.notification.data?.url || '/';
+
+  event.waitUntil(
+    clients.openWindow(url)
+  );
+});
