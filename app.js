3398f98e0b4e6e8967b1f3433db14c9ffa057e42
Dalechap <dale.chapple22@gmail.com>
2026-09-21 12:56:02 +0700

    Update app.js
    Add VAPID key conversion helper

--- app.js
+++ app.js
@@ -890,6 +890,23 @@ ${aUsed?`<small>Used</small>`:''}
 </div>`;
 }
 
+function urlBase64ToUint8Array(base64String){
+  const padding=
+    '='.repeat((4-base64String.length%4)%4);
+
+  const base64=
+    (base64String+padding)
+      .replace(/-/g,'+')
+      .replace(/_/g,'/');
+
+  const rawData=window.atob(base64);
+
+  return Uint8Array.from(
+    [...rawData].map(
+      char=>char.charCodeAt(0)
+    )
+  );
+}
 
 async function enableNotifications(){
   if(!('Notification' in window)){
